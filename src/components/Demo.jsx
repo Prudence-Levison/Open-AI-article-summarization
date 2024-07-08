5f8e7ead73599cbe5bd02469f29f1ced16610c37
Prudence-Levison <uzoekwelevisoncgmail.com>
2024-07-08 16:26:47 +0100

    Completed the UI and started the logic to the code

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import { useEffect, useState } from "react"
+import { copy, linkIcon, loader, tick } from "../assets"
 const Demo = () => {
+  const [article, setArticle] =useState( {
+    url:'',
+    summary: '',
+  })
+  
+  const handleSubmit = async (e) =>{
+  alert('submitted')
+  }
+  
   return (
-    <div>Demo</div>
-  )
+
+    <section className="mt-16 w-full max-w-xl">
+
+      <div className="flex flex-col w-full gap-2">
+        {/* Search */}
+        <form 
+        className="relative flex justify-center items-center"
+        onSubmit={handleSubmit}>
+
+          <img src={linkIcon}
+          alt="link_Icon" 
+          className="absolute left-2 my-2 my-3 w-5"/>
+
+          <input
+          type="url"
+          placeholder="Enter a url" 
+          value={article.url}
+          onChange={(e) => setArticle({... article, url:e.target.value})}
+          required
+          className="url_input peer"/>
+
+          <button
+          type="submit"
+          className="submit_btn peer-focused:border-gray-700 peer-focused:text-gray-700"
+      >  </button>
+        </form>
+        {/* Browser History*/}
+      </div>
+    </section>  )
 }
 
 export default Demo
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,18 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
 
-    <nav className='flex justify-between items-center flex-col'>
-        <img src={logo} alt='Sumz_Logo'/>
-
+    <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt='Sumz_Logo' className="w-28 object-contain"/>
+       <button type='button'
+       onClick={() => window.open('https://github.com/Prudence-Levison')}  
+       className='black_btn'>GitHub</button>
     </nav>
+
+    <h1 className='head_text'> Summarise Articles with <br className='max-md:hidden'/>
+    <span className='orange_gradient'>OpenAI GPT-4</span></h1>
+
+    <h2 className='desc'>
+      Simplify your reading with Summize, an open-source article summarizer that transforms lengthy artcles into clear and comcise summaries'</h2>
     </header>
   )
 }
